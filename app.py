516fd613a88964dc11f140965a7a4d40cccb7661
baylee-rl <bjk9@rice.edu>
2021-09-18 18:57:55 -0500

    Day 2 -- Test deploying

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
-from flask import Flask, render_template
-# import functions
-
+from flask import Flask, render_template, redirect, url_for, Response, request, session
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import io
+import os
+from functions import *
 app = Flask(__name__)
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # ROUTES #
 @app.route('/')
 def home_redirect():
     return redirect(url_for('home'))
 
-@app.route("/home")
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    state_names = ["Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+    session['state'] = 'Oregon'
 
-# @app.route("/about")
-# def about():
-# return render_template("about.html")
+    if request.method == 'POST':
+        session['state'] = request.form.get('state')
+
+        return render_template("home.html", states=state_names)    
+
+    return render_template("home.html", states=state_names)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/plot.png")
+def plot_png():
+    fig = create_figure(session['state'])
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -2,20 +2,25 @@
 create functions here :)
 """
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np
+import math
+mpl.use('Agg')
 
 
-# air quality
+# air quality dataset cleaning
 aq_df = pd.read_csv('static/datasets/airquality.csv')
 
-to_drop_aq = ["Good Days", "Moderate Days", "Unhealthy for Sensitive Groups Days", "Days CO","Days NO2","Days Ozone","Days SO2","Days PM2.5","Days PM10"]
+to_drop_aq = ["Good Days", "Moderate Days", "Days CO","Days NO2","Days Ozone","Days SO2","Days PM2.5","Days PM10"]
 aq_df.drop(to_drop_aq, inplace=True, axis=1)
 
 aq_df['County'] = aq_df['County'].str.replace('"', '')
 
-# median income
+aq_df['Bad Days'] = aq_df.iloc[:, -7:-4].sum(axis=1)
+
+
+# median income dataset cleaning
 inc_df = pd.read_csv('static/datasets/income.csv')
 
 to_drop_inc = ["FIPS_Code"]
@@ -23,10 +28,13 @@ inc_df.drop(to_drop_inc, inplace=True, axis=1)
 
 inc_df = inc_df[inc_df["Attribute"] == 'Median_Household_Income_2019']
 
-inc_df = inc_df[inc_df['Area_name'].str.contains("County")]
+inc_df = inc_df[inc_df['Area_name'].str.contains("County|Parish")]
 inc_df['County'] = inc_df['Area_name'].str.split(' County').str[0]
+inc_df['County'] = inc_df['County'].str.split(' Parish').str[0]
 
-# demographics
+
+# demographics data cleaning (currently unused)
+"""
 dem_df = pd.read_csv('static/datasets/demographics.csv', encoding='latin-1')
 
 to_drop_dem = ["WAC_MALE", "WAC_FEMALE", "BAC_MALE", "BAC_FEMALE", "IAC_MALE", "IAC_FEMALE", "AAC_MALE", "AAC_FEMALE", "NAC_MALE", "NAC_FEMALE", "NH_MALE", "NH_FEMALE", "NHWA_MALE"
@@ -56,57 +64,119 @@ for i in range(0, len(columns), 2):
 
 to_drop_dem2.append("SUMLEV")
 dem_df.drop(to_drop_dem2, inplace=True, axis=1)
+"""
 
-# print(dem_df.head())
+# graphs
+
+# filter datasets by STATE (air quality uses full name, income uses state code)
+
+# graph variables
+
+first_bar_label = '90th Percentile AQI'
+first_bar_color = '#365367'
+
+second_bar_label = 'Median Household Income (1000s)'
+second_bar_color = '#F494AB'
+
+width = 0.4  # the width of the bars
+title_size = 14
+filename = 'barh-plot'
+
+state_codes = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+}
+
+def create_figure(state):
+    """
+    Creates plot based on current selected U.S. state
+    """
+    plot_title = 'Median Household Income vs. 90th Percentile AQI by County (' + state + ')'
+
+    # filters datasets by state
+    aq_df_state = aq_df[aq_df['State'] == state]
+    inc_df_state = inc_df[inc_df['State'] == state_codes[state]]
+
+    # merge datasets based on county name
+    aq_v_inc = aq_df_state.merge(inc_df_state, on='County')
+
+    # sort by median income
+    aq_v_inc.sort_values(by='Value', inplace=True, ascending=True)
+
+    first_bar = aq_v_inc['90th Percentile AQI']
+    second_bar = aq_v_inc['Value'] / 1000
+    labels = aq_v_inc['County']
+
+    fig, ax = plt.subplots(figsize = (8,6))
+    plt.tight_layout()
+
+    # Plot double bars
+    x = np.arange(len(labels))  # Label locations
+    ax.bar(x + width/2, first_bar, width, label=first_bar_label, color=first_bar_color)
+    ax.bar(x - width/2, second_bar, width, label=second_bar_label, color=second_bar_color)
 
 
+    # Set title
+    title = plt.title(plot_title, pad=20, fontsize=title_size)
+    title.set_position([.5, 1])
 
-# education
+    # Adjust subplots
+    plt.subplots_adjust(left=0.1, top=0.85, bottom=0.3)
 
-# population
+    # Set y-labels and legend
+    ax.set_xticklabels(labels)
+    ax.legend()
 
-#graphs
+    # To show each x-label, not just even ones
+    print(x)
+    plt.xticks(np.arange(min(x), max(x)+1, 1.0), rotation=90, fontsize=(24 / math.log(len(x))))
 
-aq_df = aq_df[aq_df['State'] == 'Alabama']
-
-# print(aq_df.head())
-
-inc_df = inc_df[inc_df['State'] == 'AL']
-
-# print(inc_df.head())
-
-aq_v_inc = aq_df.merge(inc_df, on='County')
-
-print(aq_v_inc.head())
-
-# data = pd.DataFrame(aq_v_inc, columns=["County","Median AQI","Median Household Income"])
-# data_sorted = data.sort_values(by="Median Household Income", ascending=False)
-# data_sorted.set_index("County", inplace=True)
-# ranking = data_sorted.head(10)
-# ranking
-
-# first_bar = ranking["State"]
-# first_bar_label = 'Revenue, USD million'
-# first_bar_color = '#32628d'
-# second_bar = ranking['budget']
-# second_bar_label = 'Budget, USD million'
-# second_bar_color = '#cde01d'
-# labels = ranking.index
-# width = 0.4  # the width of the bars
-# plot_title = 'Top 10 movies by revenue, USD million'
-# title_size = 18
-# subtitle = 'Source: Kaggle / The Movies Dataset'
-# filename = 'barh-plot'
-
-
-# texas_counties = []
-
-# index = ["Autugua"]
-# dem_df_values = pd.DataFrame({"Median Household Income":[60], "Air Quality Index":[30]}, index=index)
-# ax = dem_df_values.plot.bar(rot=0)
-
-
-# join on airquality & income: by county (filter by state first -- can have same counties)
-# aq_df = aq_df[aq_df['State'] == 'Alabama']
-# inc_df = inc_df[inc_df['State'] == 'AL']
-# aq_v_inc = pd.merge(aq_df, inc_df)
+    return fig
